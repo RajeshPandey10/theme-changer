6308c1ff3a2edcfa536f54e98ff5ae007bafdc93
RajeshPandey10 <imrajesh2005@gmail.com>
2025-06-22 20:45:37 +0545

    the demo and the package is here:

--- first-theme/src/components/Header.jsx
+++ first-theme/src/components/Header.jsx
@@ -3,18 +3,24 @@ import ThemeToggle from "./ThemeToggle";
 
 const Header = () => {
   return (
-    <header>
-      <h1>Rajesh Pandey</h1>
+    <header className="flex justify-between items-center p-4">
+      <h1 className="text-xl font-bold">Rajesh Pandey</h1>
       <nav>
-        <ul>
+        <ul className="flex space-x-6">
           <li>
-            <a href="#about">About</a>
+            <a href="#about" className="hover:underline">
+              About
+            </a>
           </li>
           <li>
-            <a href="#projects">Projects</a>
+            <a href="#projects" className="hover:underline">
+              Projects
+            </a>
           </li>
           <li>
-            <a href="#contact">Contact</a>
+            <a href="#contact" className="hover:underline">
+              Contact
+            </a>
           </li>
         </ul>
       </nav>
